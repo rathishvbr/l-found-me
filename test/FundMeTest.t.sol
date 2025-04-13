19a117851036d2c4b27238a417cca908c86c1948
Rathish <rathishvbr@gmail.com>
2025-04-13 16:25:55 +0200

    Add gas usage tracking and new test for cheaper withdrawal from multiple funders in FundMeTest

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,6 +11,8 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_LIMIT = 3000000;
+    
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
@@ -57,8 +59,13 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = address(fundMe.getOwner()).balance;
 
         //Act
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_LIMIT);
         vm.prank(fundMe.getOwner());
         fundMe.withdraw();
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log("gasUsed", gasUsed);
 
         //Assert
         assertEq(address(fundMe).balance, 0);
@@ -84,6 +91,32 @@ contract FundMeTest is Test {
         //Act
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        //Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(address(fundMe.getOwner()).balance, startingOwnerBalance + startingFundMeBalance);
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public doFund {
+        //Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank(USER);
+            //fundMe.fund{value: SEND_VALUE}();
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
         vm.stopPrank();
 
         //Assert
